d00200a677ca8e3a0142e139285a460a2b81c692
Gustavo <gustavocop14dos@gmail.com>
2022-12-29 23:23:31 -0300

    added movement to the jump ball scene

--- Assets/Scripts/ApplyForce.cs
+++ Assets/Scripts/ApplyForce.cs
@@ -16,8 +16,15 @@ public class ApplyForce : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (
+            Input.GetKeyDown(KeyCode.Space)
+            & this.gameObject.CompareTag("ball")
+    	) {
             ball.AddForce(new Vector2(0, force * Time.deltaTime), ForceMode2D.Impulse);
         }
+        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(h * Time.deltaTime, v * Time.deltaTime, 0));
     }
 }
